cd60832d7fc152dd98d553af50469826a36e06f9
Andrii Yermolenko <vakt159@gmail.com>
2025-10-24 09:07:47 +0300

    made requested changes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int,
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
                  brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
@@ -7,16 +9,19 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: int,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, list_of_cars: [Car]) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
-        for car in list_of_cars:
+        for car in cars:
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
@@ -35,8 +40,3 @@ class CarWashStation:
         total = self.average_rating * self.count_of_ratings + rating
         self.count_of_ratings += 1
         self.average_rating = round(total / self.count_of_ratings, 1)
-
-
-car_wash_station = CarWashStation(6, 7, 3.9, 11)
-car_list = [Car(3, 3, "BMW"), Car(4, 5, "Audi"), Car(7, 1, "Mercedes")]
-car_wash_station.serve_cars(car_list)
